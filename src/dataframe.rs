fdd79e148dd1e4ceaf29bf82908dd09af7826a5d
Walther Chen <walther.chen@gmail.com>
2018-04-12 18:48:08 -0400

    IT WORKS: traits over enums
    instead of taking just a enum or trait based approach, it looks like I
    can use both to cover what I need for dynamic dispatch.
    
    Enums allow me to have an ergonomic interface for different datatypes,
    and also limits the types to what the library implements (no surprises).
    
    Traits allow for overloading functions. This is especially important for
    `apply` functions, which have to know the primitive type of the
    Datatype.
    
    My strategy is to have a trait MyTrait<T>, and then implement specific
    instances `MyTrait<i32>`, etc., for the enum. Then the implementation
    instance can check that it's routing to the proper enum value with
    primitive type, and return an error if the routing is done incorrectly.
    
    e.g. the implementation for MyEnum of MyTrait<i32> will check that the
    enum value being acted on at that moment is the right fit, a
    `MyEnum::ValueI32(Buffer<i32>)`, and if it isn't (meaning that some
    other enum value is trying to be matched on, it will return a runtime
    error.
    
    --
    No easy way to maintain a heterogenous collection while also being able
    to access the primitive type, unless it's enum. If using structs, there
    will be a tendancy for the primitive type to become a bound on the
    collection. So the enum works best for this. (Enums are good for things
    only known at runtime).
    
    And then the trait overlays this.

--- src/dataframe.rs
+++ src/dataframe.rs
@@ -12,22 +12,20 @@ impl DataFrame {
     {
         self.columns.push(new_col);
     }
-
-//    pub fn melt(self) -> DataFrame {
-//    }
-//    pub fn get_col(self) -> DataFrame {
-//    }
-//    pub fn pivot(self) -> DataFrame {
-//    }
-//    pub fn read_csv(self) -> DataFrame {
-//    }
-//    implement display
 }
 
 #[derive(Debug)]
 pub struct Column {
     name: String,
     data: Array,
+}
+
+pub trait DataType<T> {
+    fn apply_inplace<F>(&mut self, f: F) -> Result<(), Error>
+        where F: Fn(&mut T) + Sync + Send;
+
+    fn apply<F>(&self, f: F) -> Result<Array, Error>
+        where F: Fn(&T) -> T + Sync + Send;
 }
 
 /// This indirection allows for different generic types to
@@ -39,29 +37,57 @@ pub enum Array {
     UInt8(ArrayData<u8>),
 }
 
+impl Array {
+    pub fn dtype(&self) -> String {
+        match *self {
+            Array::Int8(_) => "Int8".to_owned(),
+            Array::UInt8(_) => "UInt8".to_owned(),
+        }
+    }
+}
+
 // Use macros to handle all the different types
 // basic type and Array type
-impl Array {
-    pub fn apply_inplace<F>(&mut self, f: F)
+impl DataType<i8> for Array {
+    fn apply_inplace<F>(&mut self, f: F) -> Result<(), Error>
         where F: Fn(&mut i8) + Sync + Send,
     {
         match *self {
             Array::Int8(ref mut array_data) => array_data.apply_inplace(f),
-            // TODO stuck here... can't dispatch dynamically here,
-            _ => (), // doesn't need to return error because simply doesn't compile
+            _ => return Err(format_err!("Fn type is i8 but array is {}", self.dtype())),
         }
+        Ok(())
     }
 
-    pub fn apply<F>(&self, f: F) -> Array
+    fn apply<F>(&self, f: F) -> Result<Array, Error>
         where F: Fn(&i8) -> i8 + Sync + Send,
     {
         match *self {
-            Array::Int8(ref array_data) => Array::Int8(array_data.apply(f)),
-            _ => Array::Int8(ArrayData(vec![])), // doesn't need to return error because simply doesn't compile? TODO
+            Array::Int8(ref array_data) => Ok(Array::Int8(array_data.apply(f))),
+            _ => Err(format_err!("Fn type is i8 but array is {}", self.dtype())),
         }
     }
-//    pub fn split_str(self) -> DataFrame {
-//    }
+}
+
+impl DataType<u8> for Array {
+    fn apply_inplace<F>(&mut self, f: F) -> Result<(), Error>
+        where F: Fn(&mut u8) + Sync + Send,
+    {
+        match *self {
+            Array::UInt8(ref mut array_data) => array_data.apply_inplace(f),
+            _ => return Err(format_err!("Fn type is u8 but array is {}", self.dtype())),
+        }
+        Ok(())
+    }
+
+    fn apply<F>(&self, f: F) -> Result<Array, Error>
+        where F: Fn(&u8) -> u8 + Sync + Send,
+    {
+        match *self {
+            Array::UInt8(ref array_data) => Ok(Array::UInt8(array_data.apply(f))),
+            _ => Err(format_err!("Fn type is u8 but array is {}", self.dtype())),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -104,7 +130,6 @@ mod test {
             ]
         };
         println!("{:?}", df);
-        panic!();
     }
 
     #[test]
@@ -124,63 +149,70 @@ mod test {
         println!("{:?}", df);
         let new_col = Column {
             name: "new_col".to_owned(),
-            data: df.columns[1].data.apply(|&x| x-2),
+            data: df.columns[1].data.apply(|&x: &i8| x-2).unwrap(),
         };
         df.assign(new_col);
         println!("{:?}", df);
-        panic!();
     }
 
     #[test]
     fn test_array_apply_inplace() {
         let mut array = Array::Int8(ArrayData(vec![1,2,3]));
-        array.apply_inplace(|x| *x = *x*2);
+        array.apply_inplace(|x: &mut i8| *x = *x*2).unwrap();
         println!("{:?}", array);
         fn test_fn(x: &mut i8) {
             *x = x.pow(2);
         }
-        array.apply_inplace(test_fn);
+        array.apply_inplace(test_fn).unwrap();
         println!("{:?}", array);
-        panic!();
     }
 
     #[test]
     fn test_array_apply() {
         let array = Array::Int8(ArrayData(vec![1,2,3]));
-        let array1 = array.apply(|&x| x*2);
+        let array1 = array.apply(|&x: &i8| x*2).unwrap();
         println!("{:?}", array1);
-        fn test_fn(x: &i8) -> i8 {
+        fn test_fn1(x: &i8) -> i8 {
             x.pow(2)
         }
 
-        // Test to see if this compiles
-        //fn test_fn_bad(x: String) -> String {
-        //    x.trim().to_owned()
-        //}
-        //let array2 = array.apply(test_fn_bad);
-
-        let array2 = array.apply(test_fn);
+        let array2 = array.apply(test_fn1).unwrap();
         println!("{:?}", array2);
-        panic!();
-    }
 
-    #[test]
-    fn test_array_apply_u8() {
         let array = Array::UInt8(ArrayData(vec![1,2,3]));
-        let array1 = array.apply(|&x| x*2);
+        let array1 = array.apply(|&x: &u8| x*2);
         println!("{:?}", array1);
-        fn test_fn(x: &u8) -> u8 {
+        fn test_fn2(x: &u8) -> u8 {
             x.pow(2)
         }
 
-        // Test to see if this compiles
-        //fn test_fn_bad(x: &i8) -> i8 {
-        //    x.pow(2)
-        //}
-        //let array2 = array.apply(test_fn_bad);
-
-        let array2 = array.apply(test_fn);
+        let array2 = array.apply(test_fn2).unwrap();
         println!("{:?}", array2);
-        panic!();
     }
+
+    // Need to do all the runtime tests for wrong types,
+    // since compiler can't pick up
+    #[test]
+    #[should_panic]
+    fn test_array_apply_wrong_type_i8() {
+        let array = Array::Int8(ArrayData(vec![1,2,3]));
+
+        // Test to see if this compiles
+        fn test_fn_bad1(x: &u8) -> u8 {
+            x.pow(2)
+        }
+        let array2 = array.apply(test_fn_bad1).unwrap();
+ 
+    }
+    #[test]
+    #[should_panic]
+    fn test_array_apply_wrong_type_u8() {
+        let array = Array::UInt8(ArrayData(vec![1,2,3]));
+        // Test to see if this compiles
+        fn test_fn_bad2(x: &i8) -> i8 {
+            x.pow(2)
+        }
+        let array2 = array.apply(test_fn_bad2).unwrap();
+    }
+
 }
